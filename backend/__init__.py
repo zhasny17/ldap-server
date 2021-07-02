e54cdc43b106d92c185245c9e54aefdfc8077255
Lucas <lucasluzfeitosa@fortalsistemas.com.br>
2021-07-02 15:06:21 -0300

    initial commit

---
+++ backend/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .blueprints import user
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    if config:
+        app.config.from_mapping(**config)
+
+    app.register_blueprint(user.bp, url_prefix='/users')
+
+    return app
